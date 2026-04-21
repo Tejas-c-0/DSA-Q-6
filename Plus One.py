1732c6b688ea85e46c01100fd512f985951170fe
Tejas C <shobha2ganesh@gmail.com>
2026-04-21 23:27:40 +0530

    Plus One

---
+++ Plus One.py
@@ -0,0 +1,9 @@
+digits = [1,2,3]
+def plusone(digits):
+    for i in range(len(digits) -1, -1, -1):
+        if digits[i] < 9:
+            digits[i] += 1
+            return digits
+        digits[i] = 0
+    return [1] + digits
+print(plusone(digits))    
